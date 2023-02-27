0f8da6d7a4720e9d33801959f86795f963794edc
Paulo Filipe <filipemsilva@outlook.com.br>
2023-02-27 14:47:01 -0300

    v5 - Memory leak fix + minor refactoring + TestFiles fix

--- Lab1-Merge-de arquivos/merge.c
+++ Lab1-Merge-de arquivos/merge.c
@@ -65,13 +65,15 @@ int main(int argc, char *argv[]){
     FILE *output_file = open_file(output, "wb");
 
     for ever{
-        long long int size1, size2;
         char *str1, *str2;
+        long long int size1, size2;
+        
         // Reads the size of the string from file 1
         if (fread(&size1, sizeof(long long int), 1, fileptr1) != 1){ // If it can't read, it means it reached the end of the file
             while (fread(&size2, sizeof(long long int), 1, fileptr2) == 1){ // So, copy the rest of the file 2 to the output file
                 fwrite(&size2, sizeof(long long int), 1, output_file);
-                str2 = malloc(size2);
+                str2 = malloc(size2 + 1);
+                str2[size2] = '\0';
                 fread (str2, sizeof(char), size2, fileptr2)   ;
                 printf("%s\n", str2);
                 fwrite(str2, sizeof(char), size2, output_file);
@@ -80,17 +82,13 @@ int main(int argc, char *argv[]){
             break;
         }
 
-        // Reads the string from file 1
-        str1 = malloc(size1);
-        fread(str1, sizeof(char), size1, fileptr1);
-
+        // Reads the size of the string from file 2
         if (fread(&size2, sizeof(long long int), 1, fileptr2) != 1){
-            fwrite(&size1, sizeof(long long int), 1, output_file);
-            fwrite(str1, sizeof(char), size1, output_file);
-            free(str1);
+            // If it can't read, it means it reached the end of the file
             while (fread(&size1, sizeof(long long int), 1, fileptr1) == 1){
                 fwrite(&size1, sizeof(long long int), 1, output_file);
-                str1 = malloc(size1);
+                str1 = malloc(size1 + 1);
+                str1[size1] = '\0';
                 fread (str1, sizeof(char), size1, fileptr1)   ;
                 printf("%s\n", str1);
                 fwrite(str1, sizeof(char), size1, output_file);
@@ -99,7 +97,14 @@ int main(int argc, char *argv[]){
             break;
         }
 
-        str2 = malloc(size2);
+        // Reads the string from file 1
+        str1 = malloc(size1 + 1);
+        str1[size1] = '\0';
+        fread(str1, sizeof(char), size1, fileptr1);
+
+        // Reads the string from file 2
+        str2 = malloc(size2 + 1);
+        str2[size2] = '\0';
         fread(str2, sizeof(char), size2, fileptr2);
 
         // Compare the strings
@@ -108,19 +113,19 @@ int main(int argc, char *argv[]){
             printf("%s\n", str1);
             fwrite(&size1, sizeof(long long int), 1, output_file);
             fwrite(str1, sizeof(char), size1, output_file);
-            free(str1);
             // Bring the pointer of file 2 back to the position of the string
             fseek(fileptr2, -sizeof(long long int), SEEK_CUR);
-            fseek(fileptr2, -size2, SEEK_CUR); 
+            fseek(fileptr2, -size2, SEEK_CUR);
         } else {
             printf("%s\n", str2);
             fwrite(&size2, sizeof(long long int), 1, output_file);
             fwrite(str2, sizeof(char), size2, output_file);
-            free(str2);
 
             fseek(fileptr1, -sizeof(long long int), SEEK_CUR);
             fseek(fileptr1, -size1, SEEK_CUR);
         }
+        free(str1);
+        free(str2);
     }
 
     // Closes files and frees memory
@@ -130,9 +135,9 @@ int main(int argc, char *argv[]){
     free  (output);
     free  (file1) ;
     free  (file2) ;
-
     return 0;
 }
+
 
 // Function that opens a file and checks if it was opened correctly, returns a pointer to the file
 FILE* open_file(char *file_name, char *mode){
